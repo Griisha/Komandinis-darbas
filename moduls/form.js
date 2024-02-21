34a544f76f82f3712941dd609aba97b3186aaac3
RutaMisiukeviciute <ruta.misiukeviute@gmail.com>
2024-02-21 14:00:37 +0200

    form reset

--- moduls/form.js
+++ moduls/form.js
@@ -1,11 +1,13 @@
 function formSubmit() {
   document.querySelector(".formForMessages > form").addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("here");
     const par = document.createElement("p");
     const parText = document.createTextNode("Thank you! We got your message and gonna send you email in 72 h.");
     par.append(parText);
     document.querySelector(".formForMessages").append(par);
+    e.target.elements.email.value = "";
+    e.target.elements.message.value = "";
+
   });
 
 }
